6327d516ecc69bf63c9bb60dfffea75ee0cb0c7b
XARI <xaribeforeko@gmail.com>
2025-01-30 15:37:17 -0800

    2nd Day

---
+++ Prototype 3/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravityModifier = 9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
